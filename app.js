2bfca4c07fa1bb5e6c306e706b77b9e73e1e2712
SanalThuruvelil <sanalsunny16@gmail.com>
2023-09-24 19:45:30 -0700

    sign out completed. session and cookies are working properly. removed unwanted codes.

--- app.js
+++ app.js
@@ -6,44 +6,44 @@ var createError = require('http-errors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session')
-const bodyParser = require('body-parser');
-
-
-
-var indexRouter = require('./routes/index');
-var homeRouter = require('./routes/home');
+const app = express();
 const { error } = require('console');
 
-const app = express();
+//route setup
+var indexRouter = require('./routes/index');
+var homeRouter = require('./routes/home');
+
 
 const sessionMiddleware = session({
-  secret: 'secret',
+  secret: 'Key',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  cookie: { maxAge: 600000 }
 });
 
-app.use(sessionMiddleware);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
+//view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());  
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(sessionMiddleware);
 
 app.use('/', indexRouter);
 app.use('/home', homeRouter);
 
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
 
-app.listen(3000,()=>console.log("server started"));
+
+
+
+
+app.listen(3000, () => console.log("server started"));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,40 @@
 var express = require('express');
-const alert = require('alert')
 var router = express.Router();
-const app = express();
+
+router.get('/', function (req, res, next) {
+  res.render('index');
+
+});
+const credential = {
+  username: "admin",
+  password: "admin123"
+}
 
 
-router.get('/', function(req, res, next) {
-    res.render('index');
-    
-  });
-  const credential = {
-    username : "admin",
-    password : "admin123"
-  }
- 
- 
 
 // Login route
 router.post('/', (req, res) => {
-    console.log("post message received");
-    console.log(req.body);
-    if (!req.body.username || !req.body.password){
-        res.end('Both Username and Password Needed')
-    }
-    else if (req.body.username == credential.username && req.body.password == credential.password){
-        res.redirect('/home');
-    }else{
-        res.end('invalid username or password')
-    }
-  
+  var user = req.body.username;
+
+  console.log(req.body);
+  if (!req.body.username || !req.body.password) {
+    res.end('Both Username and Password Needed')
+  }
+  else if (req.body.username == credential.username && req.body.password == credential.password) {
+    req.session.loggedIn = true;
+    req.session.user = credential.username;
+    res.redirect('/home');
+
+  } else {
+    res.end('invalid username or password')
+  }
+
 });
-app.use(function(req, res, next) {
-    next(createError(404));
-  });
+
+router.get('/logOut', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
 
 
 module.exports = router;
